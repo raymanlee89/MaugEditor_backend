8309b6b828cdece602c494ab3385bfe34d454a5c
raymanlee89 <raymanlee89@gmail.com>
2024-03-11 17:07:41 +0800

    MaugVLink version

--- GPT_prompt/__init__.py
+++ GPT_prompt/__init__.py
@@ -12,13 +12,13 @@ def ask_GPT_link(formula, prose):
         seed=seed,
         messages=[
             {"role": "system", "content": "You will find the corresponding substring in the Prose to each symbol in the Formula. All the symbol in the Formula should be in $. The substring in the Prose should be in \"."},
-            {"role": "user", "content": "Formula:  $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; Prose: The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine."},
+            {"role": "user", "content": "Formula: $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\""},
             {"role": "assistant", "content": "$\theta$: \"angle\"; $a$, $b$: \"two vectors\", \"the first and second vectors\"; $<$, $>$: \"inner product\"; $||$: \"the length\"; $\cos$: \"arc cosine\""},
-            {"role": "user", "content": "Formula:  $e^{ix} = \cos(x) + i \sin(x)$; Prose: Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates."},
+            {"role": "user", "content": "Formula: $e^{ix} = \cos(x) + i \sin(x)$; Prose: \"Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates.\""},
             {"role": "assistant", "content": "$e$: \"Growth\"; $i$: \"perpendicular direction\"; $x$: \"time\"; $\cos(x)$: \"horizontal\"; $i \sin(x)$: \"vertical\""},
-            {"role": "user", "content": "Formula:  $\Pr(H|E) = \frac{\Pr(E|H)\Pr(H)}{\Pr(E|H)\Pr(H) + \Pr(E|\text{not } H)\Pr(\text{not } H)}$; Prose: The chance evidence is real (supports a hypothesis) is the chance of a true positive among all positives (true or false)"},
+            {"role": "user", "content": "Formula: $\Pr(H|E) = \frac{\Pr(E|H)\Pr(H)}{\Pr(E|H)\Pr(H) + \Pr(E|\text{not } H)\Pr(\text{not } H)}$; Prose: \"The chance evidence is real (supports a hypothesis) is the chance of a true positive among all positives (true or false)\""},
             {"role": "assistant", "content": "$\Pr$: \"chance\"; $H$: \"a hypothesis\"; $E$: \"evidence\"; $\Pr(E|H)\Pr(H)$: \"chance of a true positive\", \"true\"; $\Pr(E|\text{not } H)\Pr(\text{not } H)$: \"false\""},
-            {"role": "user", "content": "Formula:  $" + formula + "$; Prose: \"" + prose + "\""}
+            {"role": "user", "content": "Formula: $" + formula + "$; Prose: \"" + prose + "\""}
         ]
     )
     return response.choices[0].message.content
@@ -47,7 +47,7 @@ def ask_GPT_definition(formula, prose, symbols):
         seed=seed,
         messages=[
             {"role": "system", "content": "Given a Formula and its related Prose, you will find the corresponding definitions to the given Symbol. The definitions should be the substring in the Prose. Both the Symbol and the definitions can be multiple."},
-            {"role": "user", "content": "Formula: $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; \"Prose: The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Symbol: $a$, $b$"},
+            {"role": "user", "content": "Formula: $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Symbol: $a$, $b$"},
             {"role": "assistant", "content": "\"two vectors\", \"the first and second vectors\""},
             {"role": "user", "content": "Formula: $e^{ix} = \cos(x) + i \sin(x)$; Prose: \"Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates.\"; Symbol: $e$"},
             {"role": "assistant", "content": "\"Growth\""},
@@ -75,7 +75,7 @@ def ask_GPT_symbol(formula, prose, definitions):
         seed=seed,
         messages=[
             {"role": "system", "content": "Given a Formula and its related Prose, you will find the corresponding symbols to the given Definition. The symbols should be the substring in the Formula. Both the Definition and the symbols can be multiple."},
-            {"role": "user", "content": "Formula: $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; \"Prose: The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Definition: \"two vectors\", \"the first and second vectors\""},
+            {"role": "user", "content": "Formula: $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Definition: \"two vectors\", \"the first and second vectors\""},
             {"role": "assistant", "content": "$a$, $b$"},
             {"role": "user", "content": "Formula: $e^{ix} = \cos(x) + i \sin(x)$; Prose: \"Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates.\"; Definition: \"Growth\""},
             {"role": "assistant", "content": "$e$"},
