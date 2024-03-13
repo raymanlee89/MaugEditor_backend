bd4f2958acc68d5b71f1c449f8227b13d1a1a209
raymanlee89 <raymanlee89@gmail.com>
2024-03-14 01:18:22 +0800

    Add feedback conversation

--- GPT_prompt/__init__.py
+++ GPT_prompt/__init__.py
@@ -2,28 +2,131 @@ import os
 from openai import OpenAI
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+print("OPENAI_API_KEY: ", OPENAI_API_KEY)
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 seed = 123
+model = "gpt-4-0125-preview"
 
-def ask_GPT_link(formula, prose):
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+LinkPrompt = ""
+with open('GPT_prompt/LinkPrompt.txt', 'r') as f:
+    LinkPrompt = f.read()
+
+Def2SymPrompt = ""
+with open('GPT_prompt/Def2SymPrompt.txt', 'r') as f:
+    Def2SymPrompt = f.read()
+
+Sym2DefPrompt = ""
+with open('GPT_prompt/Sym2DefPrompt.txt', 'r') as f:
+    Sym2DefPrompt = f.read()
+
+def initial_prompt(target, initial, shots):
+    messages = []
+    messages.append({
+        "role": "system",
+        "content": initial
+    })
+    for shot in shots:
+        messages.append({"role": "user", "content": shot["Q"]})
+        messages.append({"role": "assistant", "content": shot["A"]})
+    messages.append({"role": "user", "content": target})
+    return messages
+
+def add_conversation(messages, conversation):
+    for i in range(len(conversation)):
+        role = "assistant"
+        if(i % 2 == 1):
+            role = "user"
+        messages.append({"role": role, "content": conversation[i]})
+    return messages
+
+def ask_GPT(messages):
+    GPT_response = client.chat.completions.create(
+        model=model,
         seed=seed,
-        messages=[
-            {"role": "system", "content": "You will find the corresponding substring in the Prose to each symbol in the Formula. All the symbol in the Formula should be in $. The substring in the Prose should be in \"."},
-            {"role": "user", "content": "Formula: $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\""},
-            {"role": "assistant", "content": "$\theta$: \"angle\"; $a$, $b$: \"two vectors\", \"the first and second vectors\"; $<$, $>$: \"inner product\"; $||$: \"the length\"; $\cos$: \"arc cosine\""},
-            {"role": "user", "content": "Formula: $e^{ix} = \cos(x) + i \sin(x)$; Prose: \"Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates.\""},
-            {"role": "assistant", "content": "$e$: \"Growth\"; $i$: \"perpendicular direction\"; $x$: \"time\"; $\cos(x)$: \"horizontal\"; $i \sin(x)$: \"vertical\""},
-            {"role": "user", "content": "Formula: $\Pr(H|E) = \frac{\Pr(E|H)\Pr(H)}{\Pr(E|H)\Pr(H) + \Pr(E|\text{not } H)\Pr(\text{not } H)}$; Prose: \"The chance evidence is real (supports a hypothesis) is the chance of a true positive among all positives (true or false)\""},
-            {"role": "assistant", "content": "$\Pr$: \"chance\"; $H$: \"a hypothesis\"; $E$: \"evidence\"; $\Pr(E|H)\Pr(H)$: \"chance of a true positive\", \"true\"; $\Pr(E|\text{not } H)\Pr(\text{not } H)$: \"false\""},
-            {"role": "user", "content": "Formula: $" + formula + "$; Prose: \"" + prose + "\""}
-        ]
+        temperature=0,
+        messages=messages
     )
-    return response.choices[0].message.content
+    return GPT_response.choices[0].message.content
 
-def parse_link_response(response):
+def ask_GPT_link(formula, prose, conversation):
+    shots = [
+        {
+            "Q": "Formula: $\\Pr(H|E) = \\frac{\\Pr(E|H)\\Pr(H)}{\\Pr(E|H)\\Pr(H) + \\Pr(E|\\text{not } H)\\Pr(\\text{not } H)}$; Prose: \"The chance evidence is real (supports a hypothesis) is the chance of a true positive among all positives (true or false)\";",
+            "A": "$\\Pr(H|E)$: \"The chance evidence is real\"; $\\Pr(E|H)\\Pr(H)$: \"the chance of a true positive\", \"true\"; $\\Pr(E|\\text{not } H)\\Pr(\\text{not } H)$: \"false\""
+        },
+        {
+            "Q": "Formula: $\\cos \\theta = \\frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\";",
+            "A": "$\\theta$: \"angle\"; $a$, $b$: \"two vectors\", \"the first and second vectors\"; $<$, $>$: \"inner product\"; $||$: \"the length\"; $\\cos$: \"arc cosine\""
+        },
+        {
+            "Q": "Formula: $e^{ix} = \\cos(x) + i \\sin(x)$; Prose: \"Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates.\";",
+            "A": "$e$: \"Growth\"; $i$: \"perpendicular direction\"; $x$: \"time\"; $\\cos(x)$: \"horizontal\"; $i \sin(x)$: \"vertical\""
+        }
+    ]
+    target = "Formula: $" + formula + "$; Prose: \"" + prose + "\";"
+    messages = initial_prompt(target, LinkPrompt, shots)
+    messages = add_conversation(messages, conversation)
+    response = ask_GPT(messages)
+    return response
+
+def ask_GPT_definition(formula, prose, symbols, conversation):
+    symbolsString = ""
+    for i in range(len(symbols)):
+        if i == 0:
+            symbolsString = symbolsString + "$" + symbols[i] + "$"
+        else:
+            symbolsString = symbolsString + ", $" + symbols[i] + "$"
+    
+    shots = [
+        {
+            "Q": "Formula: $\\Pr(H|E) = \\frac{\\Pr(E|H)\\Pr(H)}{\\Pr(E|H)\\Pr(H) + \\Pr(E|\\text{not } H)\\Pr(\\text{not } H)}$; Prose: \"The chance evidence is real (supports a hypothesis) is the chance of a true positive among all positives (true or false)\"; Symbols: $\\Pr(E|H)$;",
+            "A": "$\\Pr(E|H)$: \"The chance evidence is real\""
+        },
+        {
+            "Q": "Formula: $\\cos \\theta = \\frac{<a,b>}{||a|| \\cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Symbols: $a$, $b$;",
+            "A": "$a$, $b$: \"two vectors\", \"the first and second vectors\""
+        },
+        {
+            "Q": "Formula: $e^{ix} = \\cos(x) + i \\sin(x)$; Prose: \"Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates.\"; Symbols: $e$;",
+            "A": "$e$: \"Growth\""
+        }
+    ]
+    target = "Formula: $" + formula + "$; Prose: \"" + prose + "\"; Symbols: $" + symbolsString + ";"
+    messages = initial_prompt(target, Sym2DefPrompt, shots)
+    messages = add_conversation(messages, conversation)
+    response = ask_GPT(messages)
+    return response
+
+def ask_GPT_symbol(formula, prose, definitions, conversation):
+    definitionsString = ""
+    for i in range(len(definitions)):
+        if i == 0:
+            definitionsString = definitionsString + "\"" + definitions[i] + "\""
+        else:
+            definitionsString = definitionsString + ", \"" + definitions[i] + "\""
+    
+    shots = [
+        {
+            "Q": "Formula: $\\Pr(H|E) = \\frac{\\Pr(E|H)\\Pr(H)}{\\Pr(E|H)\\Pr(H) + \\Pr(E|\\text{not } H)\\Pr(\\text{not } H)}$; Prose: \"The chance evidence is real (supports a hypothesis) is the chance of a true positive among all positives (true or false)\"; Definitions: \"the chance of a true positive\", \"true\";",
+            "A": "$\\Pr(E|H)\\Pr(H)$: \"the chance of a true positive\", \"true\""    
+        },
+        {
+            "Q": "Formula: $\\cos \\theta = \\frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Definitions: \"two vectors\", \"the first and second vectors\";",
+            "A": "$a$, $b$: \"two vectors\", \"the first and second vectors\""
+        },
+        {
+            "Q": "Formula: $\\cos \\theta = \\frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Definitions: \"inner product\";",
+            "A": "$<$, $>$: \"inner product\""
+        }
+    ]
+    target = "Formula: $" + formula + "$; Prose: \"" + prose + "\"; Definitions: $" + definitionsString + ";"
+    messages = initial_prompt(target, LinkPrompt, shots)
+    messages = add_conversation(messages, conversation)
+    response = ask_GPT(messages)
+    return response
+
+def parse_response(response):
     links = response.split("; ")
     result = []
     for link in links:
@@ -32,60 +135,4 @@ def parse_link_response(response):
         terms = [t.replace('"', '') for t in link[split+2:].split(", ")]
         item = {"symbols": symbols, "terms": terms}
         result.append(item)
-    return result
-
-def ask_GPT_definition(formula, prose, symbols):
-    symbolsString = ""
-    for i in range(len(symbols)):
-        if i == 0:
-            symbolsString = symbolsString + "$" + symbols[i] + "$"
-        else:
-            symbolsString = symbolsString + ", $" + symbols[i] + "$"
-    
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        seed=seed,
-        messages=[
-            {"role": "system", "content": "Given a Formula and its related Prose, you will find the corresponding definitions to the given Symbol. The definitions should be the substring in the Prose. Both the Symbol and the definitions can be multiple."},
-            {"role": "user", "content": "Formula: $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Symbol: $a$, $b$"},
-            {"role": "assistant", "content": "\"two vectors\", \"the first and second vectors\""},
-            {"role": "user", "content": "Formula: $e^{ix} = \cos(x) + i \sin(x)$; Prose: \"Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates.\"; Symbol: $e$"},
-            {"role": "assistant", "content": "\"Growth\""},
-            {"role": "user", "content": "Formula: $\Pr(H|E) = \frac{\Pr(E|H)\Pr(H)}{\Pr(E|H)\Pr(H) + \Pr(E|\text{not } H)\Pr(\text{not } H)}$; Prose: \"The chance evidence is real (supports a hypothesis) is the chance of a true positive among all positives (true or false)\"; Symbol: $\Pr(E|H)\Pr(H)$"},
-            {"role": "assistant", "content": "\"chance of a true positive\", \"true\""},
-            {"role": "user", "content": "Formula:  $" + formula + "$; Prose: \"" + prose + "\"; Symbol: " + symbolsString}
-        ]
-    )
-    return response.choices[0].message.content
-
-def parse_definition_response(response):
-    result = response[1:len(response)-1].split('", "')
-    return result
-
-def ask_GPT_symbol(formula, prose, definitions):
-    definitionsString = ""
-    for i in range(len(definitions)):
-        if i == 0:
-            definitionsString = definitionsString + "\"" + definitions[i] + "\""
-        else:
-            definitionsString = definitionsString + ", \"" + definitions[i] + "\""
-    
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        seed=seed,
-        messages=[
-            {"role": "system", "content": "Given a Formula and its related Prose, you will find the corresponding symbols to the given Definition. The symbols should be the substring in the Formula. Both the Definition and the symbols can be multiple."},
-            {"role": "user", "content": "Formula: $\cos \theta = \frac{<a,b>}{||a|| \cdot ||b||}$; Prose: \"The angle between two vectors is calculated by finding the inner product between the first and second vectors and dividing by the length of each then take the arc cosine.\"; Definition: \"two vectors\", \"the first and second vectors\""},
-            {"role": "assistant", "content": "$a$, $b$"},
-            {"role": "user", "content": "Formula: $e^{ix} = \cos(x) + i \sin(x)$; Prose: \"Growth in a perpendicular direction over time is circular: here are the horizontal and vertical coordinates.\"; Definition: \"Growth\""},
-            {"role": "assistant", "content": "$e$"},
-            {"role": "user", "content": "Formula: $\Pr(H|E) = \frac{\Pr(E|H)\Pr(H)}{\Pr(E|H)\Pr(H) + \Pr(E|\text{not } H)\Pr(\text{not } H)}$; Prose: \"The chance evidence is real (supports a hypothesis) is the chance of a true positive among all positives (true or false)\"; Definition: \"chance of a true positive\", \"true\""},
-            {"role": "assistant", "content": "$\Pr(E|H)\Pr(H)$"},
-            {"role": "user", "content": "Formula: $" + formula + "$; Prose: \"" + prose + "\"; Definition: " + definitionsString}
-        ]
-    )
-    return response.choices[0].message.content
-
-def parse_symbol_response(response):
-    result = response[1:len(response)-1].split('$, $')
     return result
--- server.py
+++ server.py
@@ -3,20 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 # for LinksExtraction
 from LinksExtraction import device, NER_tokenizer, NER_model, RE_tokenizer, RE_model, NER, RE, post_processing
-from GPT_prompt import ask_GPT_link, parse_link_response, ask_GPT_definition, parse_definition_response, ask_GPT_symbol, parse_symbol_response
-
-def parse_request(request, type = "default"):
-    string = request.get_data().decode("utf-8")
-    split = string.find("<;>")
-    formula = string[:split]
-    prose = string[split+3:]
-    if type == "with item":
-        nextSplit = string.find("<;>", split+3)
-        prose = string[split+3:nextSplit]
-        items = string[nextSplit+3:].split("<,>")
-        print("items:", items)
-        return formula, prose, items
-    return formula, prose
+from GPT_prompt import ask_GPT_link, ask_GPT_definition, ask_GPT_symbol, parse_response
 
 def get_item_location(item, string):
     result = []
@@ -28,6 +15,20 @@ def get_item_location(item, string):
         start = string.find(item, end + 1)
     return result
 
+def parse_links_text(links_text, formula, prose):
+    links = []
+    for link in links_text:
+        symbol_locs = []
+        term_locs = []
+        for symbol in link["symbols"]:
+            locs = get_item_location(symbol, formula)
+            symbol_locs = symbol_locs + locs
+        for term in link["terms"]:
+            locs = get_item_location(term, prose)
+            term_locs = term_locs + locs
+        links.append({"symbols": symbol_locs, "terms": term_locs})
+    return links
+
 app = Flask(__name__)
 CORS(app)
 
@@ -35,7 +36,10 @@ CORS(app)
 def create_links():
     print("==========================NER & RE==========================")
     print("==========================New POST==========================")
-    formula, prose = parse_request(request)
+    print("formula\n", request.json)
+    content = request.json
+    formula = content["formula"]
+    prose = content["prose"]
     print("formula\n", formula)
     print("\nprose\n", prose)
     entities = NER(prose, NER_tokenizer, NER_model, device)
@@ -45,6 +49,7 @@ def create_links():
     links_in_prose = post_processing(entities, relations)
     print("\nlinks_in_prose\n", links_in_prose)
     links = []
+    rawString = ""
     for link in links_in_prose:
         term_locs = []
         # make sure that the symbol text is unique
@@ -58,75 +63,90 @@ def create_links():
             locs = get_item_location(text, formula)
             symbol_locs = symbol_locs + locs
         links.append({"symbols": symbol_locs, "terms": term_locs})
+        # create GPT style raw string
+        symbol_string = ""
+        term_string = ""
+        for symbol in symbol_locs:
+            symbol_string += "$" + symbol["text"] + "$, "
+        if(len(symbol_string) > 0):
+            symbol_string = symbol_string[:-2]
+        for term in term_locs:
+            term_string += "\"" + term["text"] + "\", "
+        if(len(term_string) > 0):
+            term_string = term_string[:-2]
+        rawString += symbol_string + ": " + term_string + "; "
+    if(len(rawString) > 0):
+        rawString = rawString[:-2]
     print("\nlinks\n", links)
     print("============================================================")
-    return jsonify(links)
+    return jsonify({"links": links, "rawString": rawString})
 
 @app.route("/links_GPT", methods=["POST"])
 def create_links_GPT():
     print("============================GPT=============================")
     print("==========================New POST==========================")
-    formula, prose = parse_request(request)
+    print("formula\n", request.json)
+    content = request.json
+    formula = content["formula"]
+    prose = content["prose"]
+    conversation = content["conversation"]
     print("formula\n", formula)
     print("\nprose\n", prose)
-    res = ask_GPT_link(formula, prose)
+    print("\nconversation\n", conversation)
+    res = ask_GPT_link(formula, prose, conversation)
     print("\nres\n", res)
-    links_text = parse_link_response(res)
+    links_text = parse_response(res)
     print("\nlinks_text\n", links_text)
-    links = []
-    for link in links_text:
-        symbol_locs = []
-        term_locs = []
-        for symbol in link["symbols"]:
-            locs = get_item_location(symbol, formula)
-            symbol_locs = symbol_locs + locs
-        for term in link["terms"]:
-            locs = get_item_location(term, prose)
-            term_locs = term_locs + locs
-        links.append({"symbols": symbol_locs, "terms": term_locs})
+    links = parse_links_text(links_text, formula, prose)
     print("\nlinks\n", links)
     print("============================================================")
-    return jsonify(links)
+    return jsonify({"links": links, "rawString": res})
 
 @app.route("/definition_GPT", methods=["POST"])
 def create_definition_GPT():
     print("============================GPT=============================")
     print("==========================New POST==========================")
-    formula, prose, symbols = parse_request(request, "with item")
+    print("formula\n", request.json)
+    content = request.json
+    formula = content["formula"]
+    prose = content["prose"]
+    symbols = content["symbol"]
+    conversation = content["conversation"]
     print("formula\n", formula)
     print("\nprose\n", prose)
     print("\nsymbols\n", symbols)
-    res = ask_GPT_definition(formula, prose, symbols)
+    print("\nconversation\n", conversation)
+    res = ask_GPT_definition(formula, prose, symbols, conversation)
     print("\nres\n", res)
-    definitions_text = parse_definition_response(res)
-    print("\ndefinitions_text\n", definitions_text)
-    definitions = []
-    for term in definitions_text:
-        locs = get_item_location(term, prose)
-        definitions = definitions + locs
-    print("\ndefinitions\n", definitions)
+    links_text = parse_response(res)
+    print("\nlinks_text\n", links_text)
+    links = parse_links_text(links_text, formula, prose)
+    print("\nlinks\n", links)
     print("============================================================")
-    return jsonify(definitions)
+    return jsonify({"link": links[0], "rawString": res})
 
 @app.route("/symbol_GPT", methods=["POST"])
 def create_symbol_GPT():
     print("============================GPT=============================")
     print("==========================New POST==========================")
-    formula, prose, definitions = parse_request(request, "with item")
+    print("formula\n", request.json)
+    content = request.json
+    formula = content["formula"]
+    prose = content["prose"]
+    definitions = content["definition"]
+    conversation = content["conversation"]
     print("formula\n", formula)
     print("\nprose\n", prose)
     print("\ndefinitions\n", definitions)
-    res = ask_GPT_symbol(formula, prose, definitions)
+    print("\nconversation\n", conversation)
+    res = ask_GPT_symbol(formula, prose, definitions, conversation)
     print("\nres\n", res)
-    symbols_text = parse_symbol_response(res)
-    print("\nsymbols_text\n", symbols_text)
-    symbols = []
-    for symbol in symbols_text:
-        locs = get_item_location(symbol, formula)
-        symbols = symbols + locs
-    print("\nsymbols\n", symbols)
+    links_text = parse_response(res)
+    print("\nlinks_text\n", links_text)
+    links = parse_links_text(links_text, formula, prose)
+    print("\nlinks\n", links)
     print("============================================================")
-    return jsonify(symbols)
+    return jsonify({"link": links[0], "rawString": res})
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
